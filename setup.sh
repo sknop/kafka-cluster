9e1225188122cdc01b6ef9ec279141b6ed5acd47
Sven Erik Knop <seknop@gmail.com>
2018-07-24 13:41:26 +0100

    added missing setup.sh to create data directories

---
+++ setup.sh
@@ -0,0 +1,11 @@
+mkdir -p data
+mkdir -p data/kafka1
+mkdir -p data/kafka2
+mkdir -p data/kafka3
+mkdir -p data/zk1/data
+mkdir -p data/zk1/tx-logs
+mkdir -p data/zk2/data
+mkdir -p data/zk2/tx-logs
+mkdir -p data/zk3/data
+mkdir -p data/zk3/tx-logs
+
